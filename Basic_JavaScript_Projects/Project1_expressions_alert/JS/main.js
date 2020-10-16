ad71421780882efffdfc7574414241beeeddcb0f
Michael Shigezumi <mshigezumi@gmail.com>
2020-10-16 00:16:12 -0700

    Starting JavaScript

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+var Alpha = "hi ", Bravo = "by", Charlie = "e ", Delta = 11036 + 1; // Assigning variables, one with an expression
+
+Bravo = Bravo + Charlie; // Concatenating strings
+
+Alpha = Alpha.fontcolor("red"); // Assigning colors
+Bravo = Bravo.fontcolor("blue");
+
+alert("This is an alert"); // Alert
+
+document.write(Alpha); // Printing variables
+document.write(Bravo);
+document.write(Delta);
