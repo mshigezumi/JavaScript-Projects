c5243c9c815b3aa4d4d64e1fa32525f1826f5483
Michael Shigezumi <mshigezumi@gmail.com>
2020-10-16 22:32:58 -0700

    Update main.js

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
-function start() {
-    var Alpha = "Hello, ";
-    Alpha += "human.";
-    document.getElementById("text").innerHTML = Alpha;
+function start() { // Creating function
+    var Alpha = "Hello, "; // Setting variable
+    Alpha += "human."; // Concatenating strings
+    document.getElementById("text").innerHTML = Alpha; // Printing Alpha to the element with the ID "text"
 }
