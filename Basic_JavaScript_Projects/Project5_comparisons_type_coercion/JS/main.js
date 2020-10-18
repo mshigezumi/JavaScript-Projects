78cc136b7b60c04cf52b5341aee6d98672ab4684
Michael Shigezumi <mshigezumi@gmail.com>
2020-10-18 00:39:28 -0700

    Project 5 Update

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,12 +1,30 @@
-function dic() { // Creating function
-    var Array = {
-        Alpha:"A",
-        Bravo:"B",
-        Charlie:"C",
-        Delta:"D"
-    }; // Setting variable and KVPs
-    delete Alpha.Charlie; // Deleting Charlie
-    document.getElementById("Dictionary").innerHTML = Array.Charlie; // Printing Array.Charlie to the element with the ID "Dictionary"
-}
+// using writeln() instead of write() so it's actually readable
+document.writeln(typeof "hi"); // outputs string
 
-document.write(typeof "hi");
+document.writeln(0/0) // outputs NaN (not a number)
+document.writeln(isNaN("hi")) // outputs true
+document.writeln(isNaN(1)) // outputs false
+document.writeln(2E310) // outputs Infinity
+document.writeln(-2E310) // outputs -Infinity
+document.writeln("1-" + 5); // outputs 1-5
+
+console.log(2+2); // outputs 4 in the console
+console.log(2==1); // outputs false in the console
+
+document.writeln(2>1) // outputs true
+document.writeln(2<1) // outputs false
+document.writeln(2==2); // outputs true
+document.writeln(2==1); // outputs false
+
+document.writeln(2===2); // outputs true
+document.writeln(2==="1"); // outputs false
+document.writeln(2==="2"); // outputs false
+document.writeln(2===1); // outputs false
+
+document.writeln(true && true); // outputs true
+document.writeln(true && false); // outputs false
+document.writeln(true || false); // outputs true
+document.writeln(false || false); // outputs false
+
+document.writeln(!false); // outputs true
+document.writeln(!true); // outputs false
