22c4b04ebfa119e2432404795bfb518890b9cc11
Michael Shigezumi <mshigezumi@gmail.com>
2020-10-17 22:59:14 -0700

    Project 4 Added

---
+++ Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function dic() { // Creating function
+    var Array = {
+        Alpha:"A",
+        Bravo:"B",
+        Charlie:"C",
+        Delta:"D"
+    }; // Setting variable and KVPs
+    delete Alpha.Charlie; // Deleting Charlie
+    document.getElementById("Dictionary").innerHTML = Array.Charlie; // Printing Array.Charlie to the element with the ID "Dictionary"
+}
