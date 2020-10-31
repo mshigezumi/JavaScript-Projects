d3eed9df92d299491e9d139c140f43537bd4e2c9
Michael Shigezumi <mshigezumi@gmail.com>
2020-10-30 23:23:31 -0700

    Update Pizza.js

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -1,15 +1,15 @@
-function getReceipt() {
+function getReceipt() { // This function displays the order and passes the running total and text on to the next function
     var text1 = "<h3>You Ordered:</h3>";
-    var runningTotal = 0;
+    var runningTotal = 0; // Adds up the total costs as it goes through the program
     var sizeTotal = 0;
     var sizeArray = document.getElementsByClassName("size");
     for (let i = 0; i < sizeArray.length; i++) {
-        if (sizeArray[i].checked) {
+        if (sizeArray[i].checked) { // Find the checked size, and add that size to the text output
             var selectedSize = sizeArray[i].value;
             text1 = text1 + selectedSize + "<br>";
-        }   
+        }
     }
-    if (selectedSize === "Personal Pizza") {
+    if (selectedSize === "Personal Pizza") { // Change base price based on pizza size
         sizeTotal = 6;
     } else if (selectedSize === "Small Pizza") {
         sizeTotal = 8;
@@ -24,27 +24,27 @@ function getReceipt() {
     console.log(selectedSize + " = $" + sizeTotal + ".00");
     console.log("size text1: " + text1);
     console.log("subtotal: $" + runningTotal + ".00");
-    getTopping(runningTotal,text1);
+    getTopping(runningTotal,text1); // Pass the running total and output text to the next function
 };
 
-function getTopping(runningTotal,text1) {
+function getTopping(runningTotal,text1) { // Adds the list and cost of the toppings to the text and running total
     var toppingTotal = 0;
     var selectedTopping = [];
     var toppingArray = document.getElementsByClassName("toppings");
     for (let j = 0; j < toppingArray.length; j++) {
-        if (toppingArray[j].checked) {
+        if (toppingArray[j].checked) { // Add the chekced toppings to the array and to the output text
             selectedTopping.push(toppingArray[j].value);
             console.log("selected topping item: (" + toppingArray[j].value + ")");
             text1 = text1 + toppingArray[j].value + "<br>";
         }
     }
     var toppingCount = selectedTopping.length;
-    if (toppingCount > 1) {
+    if (toppingCount > 1) { // One free topping, each one after adds $1 to the total
         toppingTotal = (toppingCount - 1);
     } else {
         toppingTotal = 0;
     }
-    runningTotal = (runningTotal + toppingTotal);
+    runningTotal = (runningTotal + toppingTotal); // Full cost is piza size + # of toppings - 1 (min 0)
     console.log("total selected topping items: " + toppingCount);
     console.log(toppingCount + " topping - 1 free topping = " + "$" + toppingTotal + ".00");
     console.log("topping text1: " + text1);
